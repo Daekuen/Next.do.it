7fdad6a44ebbbc75105f620a537efaaa7000458c
Daekuen <daekuenhan@gmail.com>
2023-03-08 15:30:23 +0900

    init commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// api 폴더 안에 있는것들은 http 응답을 해준다.
 
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe' });
 }
